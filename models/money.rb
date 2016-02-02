917955050753a6b36491b86c1474404c91ff42e3
Tony Rosario <tony.rosario@gmail.com>
2016-02-02 17:51:01 -0600

    Money has an amount

---
+++ models/money.rb
@@ -0,0 +1,9 @@
+class Money
+  def initialize(amount=0)
+    @amount = amount
+  end
+
+  def amount
+    @amount / 100.0
+  end
+end
---
+++ spec/money_spec.rb
@@ -0,0 +1,41 @@
+require "spec_helper"
+
+describe Money do
+  describe "#amount"  do
+    it "returns 0.00 when not initialized with a value" do
+      money = Money.new
+      expect(money.amount).to eq(0.00)
+    end
+
+    context "less than a dollar" do
+      it "returns 1 cent" do
+        money = Money.new(1)
+        expect(money.amount).to eq(0.01)
+      end
+
+      it "returns 10 cents" do
+        money = Money.new(10)
+        expect(money.amount).to eq(0.10)
+      end
+
+      it "returns 99 cents" do
+        money = Money.new(99)
+        expect(money.amount).to eq(0.99)
+      end
+    end
+
+    context "equal to one dollar" do
+      it "returns 1.00" do
+        money = Money.new(100)
+        expect(money.amount).to eq(1.00)
+      end
+    end
+
+    context "more than a dollar" do
+      it "displays dollars and cents" do
+        money = Money.new(1337)
+        expect(money.amount).to eq(13.37)
+      end
+    end
+  end
+end
