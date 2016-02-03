1f7ea2a1899e06d2757e19105dfaea103e0e0d55
Tony Rosario <tony.rosario@gmail.com>
2016-02-02 23:36:54 -0600

    WIP - changing output result to be a Hash instead of a long array...only dollars work.

--- models/change_machine.rb
+++ models/change_machine.rb
@@ -4,9 +4,12 @@ class ChangeMachine
     cents = get_cents
 
     dollars = get_dollars
-    result = process_dollars(dollars) + process_coins(cents)
+    @result = new_result_hash
 
-    return result
+    process_dollars(dollars)
+    process_coins(cents)
+
+    return @result
 
   end
 
@@ -22,10 +25,10 @@ class ChangeMachine
 
   def process_dollars(amount)
     bills = []
-    [20, 10, 5, 1].each do |bill|
+    dollar_values.keys.each do |bill|
       num_of_bills = amount / bill
       amount -= num_of_bills * bill
-      bills << num_of_bills
+      @result[dollar_values[bill]] = num_of_bills
   end
     bills
   end
@@ -42,5 +45,17 @@ class ChangeMachine
 
   def get_cents
       @amount % 100
+  end
+
+  def new_result_hash
+    Hash[:twenties, 0, :tens, 0, :fives, 0, :ones, 0, :quarters, 0, :dimes, 0, :nickels, 0, :pennies, 0]
+  end
+
+  def coin_values
+    { 25 => :quarters, 10 => :dimes, 5 => :nickels, 1 => :pennies }
+  end
+
+  def dollar_values
+    { 20 => :twenties, 10 => :tens, 5 => :fives, 1 => :ones }
   end
 end
--- spec/change_machine_spec.rb
+++ spec/change_machine_spec.rb
@@ -3,28 +3,35 @@ require "spec_helper"
 describe ChangeMachine do
   describe "#make_change" do
     context "dollars only" do
-      it "returns [0,0,0,0] for 0" do
-        expect(subject.make_change(0)).to eq([0,0,0,0,0,0,0,0])
+
+      it "returns a Hash of 0 " do
+        expected_change = { twenties: 0, tens: 0, fives: 0, ones: 0, quarters: 0, dimes: 0, nickels: 0, pennies: 0 }
+        expect(subject.make_change(0)).to eq(expected_change)
       end
 
-      it "returns an array of [0,0,0,1] for 1 dollar" do
-        expect(subject.make_change(100)).to eq([0,0,0,1,0,0,0,0])
+      it "returns a Hash with 1 dollar bill" do
+        expected_change = { twenties: 0, tens: 0, fives: 0, ones: 1, quarters: 0, dimes: 0, nickels: 0, pennies: 0 }
+        expect(subject.make_change(100)).to eq(expected_change)
       end
 
-      it "returns an array of [0,0,0,4] for 4 dollars" do
-        expect(subject.make_change(400)).to eq([0,0,0,4,0,0,0,0])
+      it "returns a Hash with 4 ones" do
+        expected_change = { twenties: 0, tens: 0, fives: 0, ones: 4, quarters: 0, dimes: 0, nickels: 0, pennies: 0 }
+        expect(subject.make_change(400)).to eq(expected_change)
       end
 
-      it "returns an array of [0,0,1,0] for 5 dollars" do
-        expect(subject.make_change(500)).to eq([0,0,1,0,0,0,0,0])
+      it "returns a Hash with 1 five" do
+        expected_change = { twenties: 0, tens: 0, fives: 1, ones: 0, quarters: 0, dimes: 0, nickels: 0, pennies: 0 }
+        expect(subject.make_change(500)).to eq(expected_change)
       end
 
-      it "returns an array of [0,1,0,0] for 10 dollars" do
-        expect(subject.make_change(1000)).to eq([0,1,0,0,0,0,0,0])
+      it "returns a Hash with a 10 dollar bill" do
+        expected_change = { twenties: 0, tens: 1, fives: 0, ones: 0, quarters: 0, dimes: 0, nickels: 0, pennies: 0 }
+        expect(subject.make_change(1000)).to eq(expected_change)
       end
 
-      it "returns an array of [1,0,0,0] for 20 dollars" do
-        expect(subject.make_change(2000)).to eq([1,0,0,0,0,0,0,0])
+      it "returns a Hash with a 20 dollar bill" do
+        expected_change = { twenties: 1, tens: 0, fives: 0, ones: 0, quarters: 0, dimes: 0, nickels: 0, pennies: 0 }
+        expect(subject.make_change(2000)).to eq(expected_change)
       end
     end
 
